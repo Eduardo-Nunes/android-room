aa8725335dccca1b86bb099ae9de0be0570e2ff8
Eduardo Nunes <eduardo.nunes@ifactory.com.br>
2018-07-13 15:17:10 -0300

    create word entity model

---
+++ app/src/main/java/com/nunes/eduardo/roomview/Word.kt
@@ -0,0 +1,17 @@
+package com.nunes.eduardo.roomview
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+import android.support.annotation.NonNull
+
+@Entity(tableName = "word_table")
+data class Word (
+
+        @PrimaryKey(autoGenerate = true)
+        val id: Int,
+
+        @NonNull
+        @ColumnInfo(name = "word")
+        val mWord: String
+)
