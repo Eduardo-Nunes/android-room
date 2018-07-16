49444e8be1c2a0a5f82a343d1cf34fbc0df84000
Eduardo Nunes <eduardo.nunes@ifactory.com.br>
2018-07-16 17:38:09 -0300

    added room callback to initialize/populate room database

--- app/src/main/java/com/nunes/eduardo/roomview/Word.kt
+++ app/src/main/java/com/nunes/eduardo/roomview/Word.kt
@@ -7,19 +7,16 @@ import android.support.annotation.NonNull
 
 @Entity(tableName = "word_table")
 data class Word (
-
-        @PrimaryKey(autoGenerate = true)
-        val id: Int,
-
+        @PrimaryKey
         @NonNull
         @ColumnInfo(name = "word_value")
         val mWord: String
 ) {
         override fun equals(other: Any?): Boolean {
                 return if (other is Word) {
-                        other.id == this.id || other.mWord == this.mWord
+                    other.mWord == this.mWord
                 } else {
-                        false
+                    false
                 }
         }
 }
--- app/src/main/java/com/nunes/eduardo/roomview/WordRoomDatabase.kt
+++ app/src/main/java/com/nunes/eduardo/roomview/WordRoomDatabase.kt
@@ -4,12 +4,15 @@ import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.content.Context
+import android.arch.persistence.db.SupportSQLiteDatabase
+import kotlinx.coroutines.experimental.launch
 
 @Database(entities = [Word::class], version = 1)
 abstract class WordRoomDatabase : RoomDatabase() {
     abstract fun wordDao(): WordDao
 
     companion object {
+        @JvmStatic
         private var INSTANCE: WordRoomDatabase? = null
 
         @JvmStatic
@@ -18,10 +21,27 @@ abstract class WordRoomDatabase : RoomDatabase() {
                 synchronized(WordRoomDatabase::class.java) {
                     INSTANCE = Room.databaseBuilder(context.applicationContext,
                             WordRoomDatabase::class.java, "word_database")
+                            .addCallback(sRoomDatabaseCallback)
                             .build()
                 }
             }
             return INSTANCE as WordRoomDatabase
+        }
+
+        @JvmStatic
+        private val sRoomDatabaseCallback = object : RoomDatabase.Callback() {
+            override fun onOpen(db: SupportSQLiteDatabase) {
+//                synchronized(WordRoomDatabase::class.java) {
+                    super.onOpen(db)
+                    launch {
+                        INSTANCE?.wordDao()?.let {
+                            it.deleteAllWords()
+                            it.insertword(Word("Hello"))
+                            it.insertword(Word("Word"))
+                        }
+                    }
+//                }
+            }
         }
     }
 }
