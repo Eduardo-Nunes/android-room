9d539f2bf9d38161c9a63aa28d04325a364bf9b3
Eduardo Nunes <eduardo.nunes@ifactory.com.br>
2018-07-16 12:47:02 -0300

    implemented an word repository to handle data in database using coroutines

--- app/src/main/java/com/nunes/eduardo/roomview/WordRepository.kt
+++ app/src/main/java/com/nunes/eduardo/roomview/WordRepository.kt
@@ -2,15 +2,44 @@ package com.nunes.eduardo.roomview
 
 import android.app.Application
 import android.arch.lifecycle.LiveData
-import android.content.Context
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
 
 class WordRepository(application: Application){
-    private lateinit var mWordDao: WordDao
-    private lateinit var mAllWords: LiveData<List<Word>>
+    private var mWordDao: WordDao
+    private var mAllWords: LiveData<List<Word>>
 
     init {
         val db = WordRoomDatabase.getDatabase(application)
         this.mWordDao = db.wordDao()
-        this.mAllWords = mWordDao.getAllWords()
+
+/*
+runBlocking {
+mAllWords = getAllWords()
+}
+*/
+
+        this.mAllWords = getAllWords()
     }
+
+    /*
+suspend fun getAllWords(): LiveData<List<Word>>{
+return withContext(DefaultDispatcher) {
+mWordDao.getAllWords()
+}
+}
+*/
+    fun getAllWords(): LiveData<List<Word>> {
+        return async {
+            mWordDao.getAllWords()
+        }.getCompleted()
+    }
+
+    fun insert(word: Word) {
+        launch {
+            mWordDao.insertword(word)
+        }
+    }
+
+
 }
